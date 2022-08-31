281d94b11511101e37119ffca5e1d16e2558f5f7
JorgeCampostanoFroiman <67806498+JorgeCampostanoFroiman@users.noreply.github.com>
2022-08-31 07:16:55 -0300

    Arreglos varios y videos nuevos subidos

--- script.js
+++ script.js
@@ -4,11 +4,30 @@ addEventListener('DOMContentLoaded', () => {
         btn_menu.addEventListener('click', () => {
             const menu_items = document.querySelector('.menu_items')
             menu_items.classList.toggle('show')
+            
         })  
     }
 })
 
-function videoslider(links){
+addEventListener('DOMContentLoaded', () => {
+    const menu_items = document.querySelector(".menu_items")
+    if (menu_items){
+        menu_items.addEventListener('click', () => {
+            const menu_items = document.querySelector('.menu_items')
+            menu_items.classList.toggle('show')
+        })  
+    }
+})
+
+function cargarvideo(links){
     document.querySelector(".slider").src = links;
 }
 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
